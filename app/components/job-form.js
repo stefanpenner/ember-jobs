e1f536f57c39db2336109e6fb61c39efcf7e1a01
Stefan Penner <stefan.penner@gmail.com>
2015-01-16 16:49:44 -0500

    the last chunk

---
+++ app/components/job-form.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'form',
+  init() {
+    this._super(...arguments);
+    this.set('job', {});
+  },
+
+  actions: {
+    save() {
+      var jobAttributes = this.get('job');
+      jobAttributes.live = true;
+      this.set('isSaving', true);
+      this.store.createRecord('job', jobAttributes).save().
+        finally(() => {
+          this.set('isSaving', false);
+          this.sendAction('jobCreated');
+      });
+    }
+  }
+});
---
+++ app/initializers/store-component.js
@@ -0,0 +1,8 @@
+export function initialize(/* container, application */) {
+  // application.inject('route', 'foo', 'service:foo');
+}
+
+export default {
+  name: 'store-component',
+  initialize: initialize
+};
